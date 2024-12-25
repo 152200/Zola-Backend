e9604e6e7372b9f59950bfdcdec68c8d38aca2f0
152200 <akhmad.badran@gmail.com>
2024-12-26 01:46:28 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const axios = require('axios')
+const dotenv = require('dotenv')
+
+
+// const productModel = require("./models/product.model");
+// const fullProductModel = require("./models/product");
+// const categoryModel = require("./models/category");
+// const orderItemModel = require("./models/order-item");
+// const orderModel = require("./models/order");
+// const userModel = require("./models/user");
+
+const app = express();
+dotenv.config()
+app.use('/uploads', express.static('uploads'));
+
+//middleware
+app.use(cors(
+  {
+    origin: [process.env.ORIGIN1, process.env.ORIGIN2, process.env.ORIGIN3, process.env.ORIGIN4],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  }
+));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+
+//routes
+// const productRoute = require("./routes/product.route");
+const categoriesRoutes = require('./routes/categories');
+const productsRoutes = require('./routes/products');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
+const newOrderRoutes = require('./routes/newOrder');
+const { CURSOR_FLAGS } = require("mongodb");
+
+//const api = process.env.API_URL;
+
+
+app.use(`/categories`, categoriesRoutes);
+app.use(`/products`, productsRoutes);
+app.use(`/users`, usersRoutes);
+app.use(`/orders`, ordersRoutes);
+app.use('/webhook', newOrderRoutes)
+
+mongoose.connect(process.env.MONGO_URI,{
+  dbName: "Zola-Sook",
+})
+  .then(() => {
+    console.log("Connected to database!");
+    
+    app.listen(3000, () => {
+      console.log(`Server Is Running On Port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection Failed");
+  });
---
+++ routes/newOrder.js
@@ -0,0 +1,178 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const nodemailer = require('nodemailer');
+const twilio = require('twilio');
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'akhmad.badran@gmail.com',
+      pass: 'yhfx qoct ceso glkb',
+    },
+  });
+
+router.post('/', async(req, res) => {
+    const order = req.body;
+    products = order.orderItems;
+    try {
+      // Find the user by ID
+      const response = await axios.get('http://localhost:3000/users'); // Adjust the URL as needed
+      const users = response.data;
+  
+      const user = users.find(u => u.id === order.user); // Adjust this logic based on your user ID field
+  
+      if (!user) {
+          return res.status(404).send('User not found');
+      }
+      // Create an HTML table for the order details
+      let htmlTable = `
+        <h3>التفاصيل</h3>
+        <table style="width: 100%; border-collapse: collapse;">
+          <tr>
+            <th style="border: 1px solid #ddd; padding: 8px;">Field</th>
+            <th style="border: 1px solid #ddd; padding: 8px;">Details</th>
+          </tr>
+          <tr>
+            <td style="border: 1px solid #ddd; padding: 8px;">User Name</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">${user.name}</td>
+          </tr>
+          <tr>
+            <td style="border: 1px solid #ddd; padding: 8px;">User Email</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">${user.email}</td>
+          </tr>
+          <tr>
+            <td style="border: 1px solid #ddd; padding: 8px;">User Phone</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">${user.phone}</td>
+          </tr>
+          <tr>
+            <td style="border: 1px solid #ddd; padding: 8px;">Shipping Address</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">${order.shippingAddress1}, ${order.shippingAddress2}, ${order.city}, ${order.zip}, ${order.country}</td>
+          </tr>
+          <tr>
+            <td style="border: 1px solid #ddd; padding: 8px;">Phone</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">${order.phone}</td>
+          </tr>
+          <tr>
+            <td style="border: 1px solid #ddd; padding: 8px;">Status</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">${order.status}</td>
+          </tr>
+          <tr>
+            <td style="border: 1px solid #ddd; padding: 8px;">Total Price</td>
+            <td style="border: 1px solid #ddd; padding: 8px;">$${order.totalPrice}</td>
+          </tr>
+        </table>
+        <br/>
+        <br/>
+        <br/>
+        
+  
+      `;
+  
+   
+      // Prepare the email options
+      const mailOptions = {
+        from: 'noreply@yourdomain.com', // Default sender
+        to: 'akhmad.badran@gmail.com',
+        subject: 'طلب جديد',
+        html: htmlTable, // Use the HTML table as the email body
+      };
+  
+      // Send the email
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Email sent: ' + info.response);
+      });
+  
+      res.status(200).send('Order received and email sent');
+  
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+  // webhook endpoint end
+
+
+  // sending whatsapp message start.................................................................................................................................
+
+
+  const accountSid = 'ACe54244ab179e1502b86d11ae68a0855f'; // Your Twilio account SID
+  const authToken = '7939901ec57c7a01d4c0a5db5628bb2d'; // Your Twilio auth token
+  const client = twilio(accountSid, authToken);
+
+
+  router.post('/whatsapp', async (req, res) => {
+    const order = req.body;
+    const products = order.orderItems;
+    
+    try {
+        // Find the user by ID
+        const response = await axios.get('http://localhost:3000/users'); // Adjust the URL as needed
+        const users = response.data;
+    
+        const user = users.find(u => u.id === order.user); // Adjust this logic based on your user ID field
+    
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+
+        // New route to send order to WhatsApp
+         let whatsappMessage =  `
+        📦 *New Order:
+        💰 *Total Price: ${order.totalPrice}
+
+        👤 *User Information:
+        👤 *Name: ${user.name}
+        📧 *Email: ${user.email}
+        📞 *Phone: ${user.phone}
+        🏠 *Shipping Address: ${user.city}, ${user.city},
+
+
+        The Order :
+        `;
+
+        // Append orderItems details to the message
+        order.orderItems.map((item, index) => {
+        whatsappMessage += `
+        🛒 *Item ${index + 1}:*
+        📦 *Product Name:* ${item.product.name}
+        🔢 *Quantity:* ${item.quantity}
+        💵 *Price:* ${item.product.price}  
+  `;
+});
+
+
+        // Send WhatsApp message
+        client.messages.create({
+            from: 'whatsapp:+14155238886', // Twilio WhatsApp number
+            to: 'whatsapp:+972598817229', // The recipient's WhatsApp number
+            body: whatsappMessage,
+        })
+        .then(message => console.log('WhatsApp message sent: ' + message.sid))
+        .catch(error => console.error('Error sending WhatsApp message:', error));
+    
+        res.status(200).send('Order received, email and WhatsApp message sent');
+    
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
+
+
+  // sending whatsapp message end...........................................................................................................................
+
+
+
+
+
+
+
+
+  module.exports =router;
